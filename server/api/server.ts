b5c2a8cf01e2238c28acf562167a4c00a3ce8dfb
Sarvesh Mohite <142711109+CyberSarvesh@users.noreply.github.com>
2026-03-24 20:42:17 +0530

    fixing of package-json mistakes adn adding cors

--- server/api/server.ts
+++ server/api/server.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import jobRoutes from "../routes/jobs.ts";
+import cors from "cors";
+
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/jobs", jobRoutes);
